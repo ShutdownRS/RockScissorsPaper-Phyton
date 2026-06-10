ecba0e1be6b8fbdbd6cef2bd8422b5288ba1dadf
Shutdown <lolnetomlet@gmail.com>
2026-06-10 20:24:48 +0500

    Add files via upload

---
+++ PockPaperScissors.py
@@ -0,0 +1,18 @@
+import random
+
+print("Welcome to the rock paper scissors game!")
+while True:
+    print("\nChoose: rock, scissors or paper")
+    player_choice = input("Your choice: ").lower()
+    if player_choice not in ["rock", "scissors", "paper"]:
+        print("Error! write: rock, paper, or scissors")
+        continue
+    result = random.choice(["win", "lose"])
+    if result == "win":
+        print("You win!")
+    else:
+        print("You loose!")
+    play_again = input("\nPlay again? (y/n): ").lower()
+    if play_again != "да":
+        print("Thanks for game! Goodbye!")
+        break 
